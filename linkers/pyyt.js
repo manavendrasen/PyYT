6099596d7275c75a06457a2104254a759cc426a4
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-06-08 00:48:00 +0530

    added python-shell

---
+++ linkers/pyyt.js
@@ -0,0 +1,19 @@
+function download() {
+  var python = require("python-shell");
+  var path = require("path");
+
+  var ytlink = document.getElementById("ytlink").value;
+  document.getElementById("ytlink").value = "Wait";
+
+  var options = {
+    scriptPath: path.join(__dirname, "/../engine/"),
+    args: [ytlink],
+  };
+
+  var pyyt = new python("run.py");
+  pyyt.on("message", (message) => {
+    console.log(message);
+  });
+}
+
+module.exports = download;
